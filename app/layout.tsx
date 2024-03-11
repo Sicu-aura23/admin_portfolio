9693dfba0249d92607da918107d7541d850d6b1c
Sicu-aura23 <155062207+Sicu-aura23@users.noreply.github.com>
2024-03-11 20:38:22 +0530

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Sicu-aura-Admin',
+  title: 'Sicu-aura-Portfolio - Admin',
   description: 'Sicu-aura : Your one stop safety solutions from India',
 };
 
